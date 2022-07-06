ca0bcc28eac49d489f9effcf43eb443636ef6cf8
Ahmed AlAmiri <a.alamiri.96@gmail.com>
2022-07-06 07:02:36 +0300

    Fix navbar and add proper routing

--- src/App.js
+++ src/App.js
@@ -1,32 +1,30 @@
 import Home from "./components/Home/Home";
-import NavBar from "./components/shared/NavBar";
-import "./App.css";
-import Navbar from "./Navbar";
+import Navbar from "./components/shared/Navbar";
 import Welcome from "./Welcome/Welcome";
 import Login from "./Login/Login";
 import Register from "./Login/Register";
 import Profile from "./Profile/Profile";
 import ResetPassword from "./Login/ResetPassword";
-import { Route, Routes, useLocation } from "react-router-dom";
-import authStore from "./stores/authStore";
+import { Route, Routes } from "react-router-dom";
+
 import { observer } from "mobx-react";
 
 function App() {
-  const location = useLocation();
-  const user = authStore.user;
-  const nav = user ? <NavBar /> : <Navbar />;
   return (
     <div className="w-screen h-screen">
-      {nav}
-
-      <Routes>
-        <Route path="/Login" element={<Login />} />
-        <Route path="/Register" element={<Register />} />
-        <Route path="/Profile" element={<Profile />} />
-        <Route path="/ResetPassword" element={<ResetPassword />} />
-        <Route path="/" element={<Welcome />} />
-        <Route path="/dashboard" element={<Home />} />
-      </Routes>
+      <div className="h-[4rem]">
+        <Navbar />
+      </div>
+      <div className="h-[calc(100%-4rem)]">
+        <Routes>
+          <Route path="/Login" element={<Login />} />
+          <Route path="/Register" element={<Register />} />
+          <Route path="/Profile" element={<Profile />} />
+          <Route path="/ResetPassword" element={<ResetPassword />} />
+          <Route path="/" element={<Welcome />} />
+          <Route path="/dashboard" element={<Home />} />
+        </Routes>
+      </div>
     </div>
   );
 }
