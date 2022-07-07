009071f1c5a1b14b25586a4ad41af5f2ec837a29
Ahmed AlAmiri <a.alamiri.96@gmail.com>
2022-07-07 08:53:52 +0300

    Fix leaderboard ranking avatar styling

--- src/Componets/Board/LeadBoard/RankLeaderboard.js
+++ src/Componets/Board/LeadBoard/RankLeaderboard.js
@@ -1,38 +1,23 @@
-import React from 'react'
+import React from "react";
 import { observer } from "mobx-react";
-import  coin  from '../../../images/coin.png'
+import coin from "../../../images/coin.png";
+import RankAvatar from "./RankAvatar";
 
-function RankLeaderboard ({member}) {
-
+function RankLeaderboard({ member }) {
   return (
- <div className='place-content-center'> 
-     <div className='  container  bg-white h-[70px] rounded-full p-2 flex  justify-between'>
-        <div className='flex items-center  space-x-10 '>
-        <div className='winner-tag2 bg-theme-yellow flex  items-center'>
-     <div className=' relative left-[10px]  text-white font-bold'>{member.rank}</div>
-        <img className='rounded-[100%] w-[60px] h-[60px] relative  left-[20px]
-        ' src='https://ichef.bbci.co.uk/news/976/cpsprodpb/FD27/production/_101970846_aubreyblanche.jpg'/>
+    <div className="place-content-center">
+      <div className="container bg-white h-[70px] rounded-full px-6 py-2 flex items-center gap-4">
+        <RankAvatar
+          rank={member.rank}
+          image="https://ichef.bbci.co.uk/news/976/cpsprodpb/FD27/production/_101970846_aubreyblanche.jpg"
+        />
+        <h1>{member.name}</h1>
+        <div className=" flex items-center p-[10px] h-[100%] w-[80px] rounded-full bg-theme-light-grey place-content-center ml-auto">
+          <span>{member.point}</span>
+          <img src={coin} />
         </div>
-        <h1  className=''> {member.name}</h1>
-        </div>
-
-        <div className=' flex items-center p-[10px] h-[100%] w-[80px] rounded-full bg-theme-light-grey place-content-center'>
-            <span>{member.point}</span>
-            <img src={coin} />
-        
-        
-        </div>
-  
-     
-     
-     </div>
-     </div>
-     
-
-
-
-  )
+      </div>
+    </div>
+  );
 }
-export default observer( RankLeaderboard 
-);
-
+export default observer(RankLeaderboard);
