797cf18a0fbb66d0023a537d8548f22d793d622b
Ahmed AlAmiri <a.alamiri.96@gmail.com>
2022-07-08 08:41:59 +0300

    Fix adding board then refreshing in user store

--- src/stores/userStore.js
+++ src/stores/userStore.js
@@ -8,8 +8,25 @@ class UserStore {
   constructor() {
     makeAutoObservable(this);
   }
-  user = null;
-  userBoards = null;
+  _user = null;
+  _userBoards = null;
+
+  set user(userData) {
+    this._user = userData;
+    this._updateStorage(this._user);
+  }
+  set userBoards(boards) {
+    this._user.boards = boards;
+    this._userBoards = boards;
+    this._updateStorage(this._user);
+  }
+
+  get user() {
+    return this._user;
+  }
+  get userBoards() {
+    return this._userBoards;
+  }
 
   updateUserStore = async (id) => {
     if (!id) return localStorage.removeItem(KEY);
@@ -31,16 +48,20 @@ class UserStore {
   setFields = (userData) => {
     this.user = userData;
     this.userBoards = userData.boards;
-    localStorage.setItem(KEY, JSON.stringify(userData));
+    this._updateStorage(userData);
   };
 
   addBoard = (board) => (this.userBoards = [...this.userBoards, board]);
+
   updateBoard = (updatedBoard) =>
     (this.userBoards = this.userBoards.map((board) =>
       board._id === updatedBoard._id ? updatedBoard : board
     ));
+
   deleteBoard = (boardId) =>
     (this.userBoards = this.userBoards.filter(({ _id }) => _id !== boardId));
+
+  _updateStorage = (user) => localStorage.setItem(KEY, JSON.stringify(user));
 }
 
 async function tryCatch(promise) {
