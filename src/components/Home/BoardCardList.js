4a52dbf3b0502d476cf4e26d7f3fa88ea7b51c30
Ahmed AlAmiri <a.alamiri.96@gmail.com>
2022-07-13 04:11:04 +0300

    clean + remove local storage from user store file
    problem:
        refreshing the page does not fetch the latest updates from the backend.
    
    cause:
        user data is being stored in local storage as long as the auth user is valid.
    
    solution:
        remove local storage and make a new request each time the user refreshes the page or opens a new session.

--- src/components/Home/BoardCardList.js
+++ src/components/Home/BoardCardList.js
@@ -8,10 +8,10 @@ import { Link } from "react-router-dom";
 
 const BoardCardList = () => {
   const cardListWidth =
-    userStore.userBoards.length > 3
+    userStore.user.boards.length > 3
       ? "w-[calc(3*15rem+8rem)]"
       : "w-[calc(3*15rem+2rem)]";
-  const cards = userStore.userBoards.map((board) => (
+  const cards = userStore.user.boards.map((board) => (
     <Link key={board._id} to={`/board/${board.slug}`} state={{ id: board._id }}>
       <Card leading={board.title} content={<BoardChart />} />
     </Link>
