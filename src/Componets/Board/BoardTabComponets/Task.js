083825231b04c4991972381631b894be25574e02
Ahmed AlAmiri <a.alamiri.96@gmail.com>
2022-07-23 01:30:28 +0300

    Only board admins can edit or delete task

--- src/Componets/Board/BoardTabComponets/Task.js
+++ src/Componets/Board/BoardTabComponets/Task.js
@@ -6,6 +6,7 @@ import AssignButton from "./AssignButton";
 import ModifyTaskButton from "./ModifyTaskButton";
 import TaskForm from "./TaskForm";
 import { observer } from "mobx-react";
+import boardStore from "../../../stores/boardStore";
 
 function Task({ task, listLength }) {
   const [showModal, setShowModal] = useState(false);
@@ -16,6 +17,7 @@ function Task({ task, listLength }) {
 
   const isDone = task.list === "done";
   const buttonDisplay = isDone ? "hidden" : "";
+  const isAdmin = boardStore.userIsAdmin();
 
   const [, drag] = useDrag(() => ({
     type: "TASK",
@@ -26,18 +28,22 @@ function Task({ task, listLength }) {
     <>
       <div
         ref={drag}
-        className="rounded-lg bg-white  p-[10px] flex flex-col gap-4 items-start group"
+        className={`rounded-lg bg-white  p-[10px] flex flex-col gap-4 items-start ${
+          isAdmin && "group"
+        }`}
       >
         <div className="w-full flex justify-between">
           <p className="text-theme-green group-hover:text-theme-green/70">
             Points: {task.points}
           </p>
-          <ModifyTaskButton
-            className={`group-hover:opacity-100 transition-opacity ${buttonDisplay}`}
-            openModal={openModal}
-            listLength={listLength}
-            deleteTask={deleteTask}
-          />
+          {isAdmin && (
+            <ModifyTaskButton
+              className={`group-hover:opacity-100 transition-opacity ${buttonDisplay}`}
+              openModal={openModal}
+              listLength={listLength}
+              deleteTask={deleteTask}
+            />
+          )}
         </div>
         <div className="flex justify-between w-full">
           <p className="text-black group-hover:text-black/70">{task.title}</p>
